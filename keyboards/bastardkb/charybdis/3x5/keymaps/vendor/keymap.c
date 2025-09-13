78e57b8e94e0652c232473156959b12c94c70ca2
Javier Rodriguez <javier.rodriguez.4224@gmail.com>
2025-09-13 14:12:24 +0200

    Update Keymap

--- keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
@@ -91,10 +91,10 @@ static uint16_t auto_pointer_layer_timer = 0;
  * from the base layer to enable auto-repeat.
  */
 #define LAYOUT_LAYER_FUNCTION                                                                 \
-    _______________DEAD_HALF_ROW_______________, KC_PSCR,   KC_F7,   KC_F8,   KC_F9,  KC_F12, \
-    ______________HOME_ROW_GACS_L______________, KC_SCRL,   KC_F4,   KC_F5,   KC_F6,  KC_F11, \
-    _______________DEAD_HALF_ROW_______________, KC_PAUS,   KC_F1,   KC_F2,   KC_F3,  KC_F10, \
-                      XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, S(KC_4), XXXXXXX, S(KC_6), S(KC_9), S(KC_0), KC_MINS, XXXXXXX, \
+    KC_LSFT, S(KC_2), S(KC_3), S(KC_5), XXXXXXX, S(KC_7), S(KC_LBRC), S(KC_RBRC), KC_EQL, KC_SCLN, \
+    KC_GRV, XXXXXXX, XXXXXXX, S(KC_SCLN), XXXXXXX, S(KC_8), KC_LBRC, KC_RBRC, S(KC_1), KC_BSLS, \
+                      KC_LGUI, _______, KC_LALT,  KC_ENT, KC_LCTL
 
 /**
  * \brief Media layer.
